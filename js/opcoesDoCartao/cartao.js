7f83d3f278904b409a84d4b56c6e4cc3706dc698
Cintia-Teixeira <cin.contaadm@gmail.com>
2020-01-22 16:57:14 -0300

    Trabalhando com remoção e validação dos cartões; aplicando jquery

--- js/opcoesDoCartao/cartao.js
+++ js/opcoesDoCartao/cartao.js
@@ -13,10 +13,26 @@
             cartao.classList.remove('cartao--focado')
         })
 
-        cartao.addEventListener('change', function(event) {
+        cartao.addEventListener('change', function (event) {
             if (event.target.classList.contains('opcoesDoCartao-radioTipo')) {
                 cartao.style.backgroundColor = event.target.value;
-            }      
+            }
+        })
+
+        cartao.addEventListener('keydown', function (event) {
+            if (event.target.classList.contains('opcoesDoCartao-opcao') && (event.key === 'Enter')) {
+                event.target.click();
+            }
+        })
+
+        cartao.addEventListener('click', function (event) {
+            if (event.target.classList.contains('opcoesDoCartao-remove')) {
+                cartao.classList.add('cartao--some');
+
+                cartao.addEventListener('transitionend', function () {
+                    cartao.remove();
+                })
+            }
         })
     }
 
---
+++ js/opcoesDoCartao/mural.js
@@ -0,0 +1,46 @@
+; (function () {
+
+    const mural = document.querySelector('.mural');
+
+
+    mural.addEventListener('click', function (event) {
+        if (event.target.classList.contains('opcoesDoCartao-radioTipo')) {
+            const cartao = event.target.closest('.cartao');
+            cartao.style.backgroundColor = event.target.value;
+        }
+    })
+
+    mural.addEventListener('keydown', function (event) {
+        if (event.target.classList.contains('opcoesDoCartao-opcao') && (event.key === 'Enter')) {
+            event.target.click();
+        }
+
+    })
+
+    mural.addEventListener('click', function (event) {
+        if (event.target.classList.contains('opcoesDoCartao-remove')) {
+            const cartao = event.target.closest('.cartao');
+
+            cartao.classList.add('cartao--some');
+
+            cartao.addEventListener('transitionend', function () {
+                cartao.remove();
+            })
+        }
+    })
+
+    mural.addEventListener('focusin', function (event) {
+        const cartao = event.target.closest('.cartao');
+        cartao.classList.add('cartao--focado');
+    })
+
+    mural.addEventListener('focusout', function (event) {
+        const cartao = event.target.closest('.cartao');
+        cartao.classList.remove('cartao--focado');
+    })
+
+
+
+
+
+})();
