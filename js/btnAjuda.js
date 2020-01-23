638cb42f98fa8cdc13710217567375b987a54d0b
Cintia-Teixeira <cin.contaadm@gmail.com>
2020-01-23 11:47:09 -0300

    Mostrando informações de ajuda em cartões

--- js/btnAjuda.js
+++ js/btnAjuda.js
@@ -3,12 +3,15 @@
     const btn = document.querySelector('#btnAjuda');
 
     btn.addEventListener('click', function () {
-        const ajudas = ["Bem Vindo ao Ceep", "Clique no btn Linhas para mudar o layout"];
+        const ajudas = [{ conteudo: "Bem Vindo ao Ceep", cor: "#DEB887" },
+                        { conteudo: "Clique no btn Linhas para mudar o layout", cor: "#D2B48C" }];
 
         ajudas.forEach(function (ajuda) {
-            alert(ajuda)
+            adicionaCartaoNoMural(ajuda)
         })
     })
+
+    
 
     btn.classList.remove('no-js');
 
